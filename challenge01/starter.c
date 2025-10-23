ed06b8e6ed332ab871791b6b57c99be2649d83ae
gemartin99 <gerardmg19953@gmail.com>
2025-10-23 02:30:09 +0200

    Version 1.0

---
+++ challenge01/starter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Declaración de la función a implementar
+int suma(int a, int b);
+
+int main() {
+    // Ejemplo de uso rápido
+    printf("Resultado: %d\n", suma(2, 3));
+    return 0;
+}
