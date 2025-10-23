21f5242d43d4c25b412c5c296b4b079fb604ebe0
Miguel Dagraza <migueldagraza@gmail.com>
2025-10-23 13:03:12 +0200

    challenge1

--- challenge01/starter.c
+++ challenge01/starter.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <assert.h>
 
 // Declaración de la función a implementar
-int suma(int a, int b);
+int suma(int a, int b){
+    return a + b;
+}
 
 int main() {
+    assert(suma(2,3) == 5);
+    assert(suma(0,0) == 0);
+    assert(suma(-1,1) == 0);
+    assert(suma(-2,-3) == -5);
     // Ejemplo de uso rápido
     printf("Resultado: %d\n", suma(2, 3));
     return 0;
---
+++ challenge01/test_challenge01.c
@@ -0,0 +1,8 @@
+/*#include <assert.h>
+#include <stdio.h>
+#include "starter.c"
+
+assert(suma(2,3) == 6);
+assert(suma(0,0) == 0);
+assert(suma(-1,1) == 0);
+assert(suma(-2,-3) == -5);*/
